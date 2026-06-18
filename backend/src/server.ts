e98651cdc948384d2e4b68dbfa61e1073229d17e
rishu <rrishu212@gmail.com>
2026-06-19 03:12:41 +0530

    Backend Setup

---
+++ backend/src/server.ts
@@ -0,0 +1,32 @@
+import { createApp } from "./app.js";
+import { env } from "./config/env.js";
+import { assertDatabaseConnection } from "./db/db.js";
+import { logger } from "./lib/logger.js";
+import http from "node:http";
+
+
+
+async function bootstrap(){
+    try {
+
+        await assertDatabaseConnection()
+
+        const app = createApp();
+        const server = http.createServer(app);
+
+        const port = Number(env.PORT) || 5000;
+
+        console.log(port);
+
+        server.listen(port,()=> {
+            logger.info(`Server is running to port: http://localhost${port}`)
+        })
+        
+    } catch (err) {
+        console.log(err);
+        logger.error('Failed to start the server', `${(err as Error).message}`)
+        process.exit(1);
+    }
+}
+
+bootstrap();
